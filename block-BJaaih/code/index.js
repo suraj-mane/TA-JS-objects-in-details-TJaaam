b6f6562e9bebd07ee04a942be53d693cf96f67fd
suraj-mane <sun.mane1100@gmail.com>
2022-02-26 22:17:32 +0530

    complete

--- block-BJaaih/code/index.js
+++ block-BJaaih/code/index.js
@@ -3,7 +3,12 @@
 `myMap` will behave similar to how the `Array.map` works. To test the implementation use the code below.
 */
 
-// myMap function goes here
+function myMap(array) {
+  this.arrray = array;
+  Array.prototype = function(callBack){
+    return callBack;
+  }
+}
 
 // Test the myMap function you created above
 
